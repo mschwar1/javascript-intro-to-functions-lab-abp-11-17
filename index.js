5eb5a77f81c31ebc8724461fa29f991a04d196ab
Mai Schwartz <mai.schwartz@gmail.com>
2017-11-13 18:46:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string) {
-  if (string.toLowerCase()=string) {
-    return "I can't hear you!" }
-  if else (string=string.toLowerCase()) {
-    return "YES INDEED!"}
-  if else (string="I love you, Grandma") {
-    return "I love you too" }
-
-}
